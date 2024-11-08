b3d49033601ce9112d4801046690b67843a2dd67
ponziot <111065521+ponziot@users.noreply.github.com>
2024-11-08 08:56:45 -0300

    08/11

--- GRAFICO/redesfav.js
+++ GRAFICO/redesfav.js
@@ -1,4 +1,19 @@
+import { getCss,tickfont } from "./comum.js";
+
 async function redesfav(){
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
-const res = ''
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json';
+    const res =  await fetch(url);
+    const dados = await res.json();
+    const redes = Object.keys(dados);
+    const valores =Object.values(dados);
+    
+    const infos = [
+    {
+        values: valores,
+        labels: redes,
+        type: 'pie',
+        textinfo: 'label+percent'
+    }
+    ];
 }
+redesfav();
